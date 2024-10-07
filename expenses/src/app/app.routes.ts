724ee42f745022a9dfbe31c0db9f8d060c7eb942
113855-Sanchez <113855@tecnicatura.frc.utn.edu.ar>
2024-10-07 14:01:08 -0300

    router

--- expenses/src/app/app.routes.ts
+++ expenses/src/app/app.routes.ts
@@ -4,6 +4,6 @@ import { LiquidationExpenseComponent } from './components/routes/liquidation-exp
 
 export const routes: Routes = [
     { path: 'expense/:id', component:LiquidationExpenseComponent },
-    { path: '', redirectTo: '', pathMatch: 'full' }, // Asegúrate de que no esté causando conflictos
+    { path: '', redirectTo: '', pathMatch: 'full' }, 
     { path: '**', redirectTo: '/expense/1' },
 ];
