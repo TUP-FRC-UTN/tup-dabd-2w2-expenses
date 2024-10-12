8d1eedfd657c69075ce055e0a238ae081aa3b5d5
113855-Sanchez <113855@tecnicatura.frc.utn.edu.ar>
2024-10-12 11:42:00 -0300

    liquidation expense form, and state close

--- expenses/src/app/components/expenses-liquidation-expense/expenses-liquidation-expense.component.ts
+++ expenses/src/app/components/expenses-liquidation-expense/expenses-liquidation-expense.component.ts
@@ -3,10 +3,11 @@ import LiquidationExpense from '../../models/liquidationExpense';
 import { LiquidationExpenseService } from '../../services/liquidation-expense.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { PeriodSelectComponent } from '../selects/period-select/period-select.component';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-expenses-liquidation-expense',
   standalone: true,
-  imports: [PeriodSelectComponent],
+  imports: [PeriodSelectComponent, CommonModule],
   templateUrl: './expenses-liquidation-expense.component.html',
   styleUrl: './expenses-liquidation-expense.component.css',
 })
@@ -14,8 +15,8 @@ export class ExpensesLiquidationExpenseComponent implements OnInit {
   liquidationExpensesService: LiquidationExpenseService = inject(
     LiquidationExpenseService
   );
-  route: ActivatedRoute = inject(ActivatedRoute); 
-  router:Router=inject(Router)
+  route: ActivatedRoute = inject(ActivatedRoute);
+  router: Router = inject(Router);
   liquidationExpensesList: LiquidationExpense[] = [];
   id: number | null = null; // Variable to store the ID
 
@@ -29,9 +30,9 @@ export class ExpensesLiquidationExpenseComponent implements OnInit {
       this.id = Number(params.get('id')); // Cast to number if necessary
       console.log('Retrieved ID:', this.id); // Debugging line
     });
-    console.log(this.liquidationExpensesList)
+    console.log(this.liquidationExpensesList);
   }
-  private loadList(id:number|null) {
+  private loadList(id: number | null) {
     if (id) {
       this.liquidationExpensesService
         .get(id)
@@ -39,10 +40,35 @@ export class ExpensesLiquidationExpenseComponent implements OnInit {
           this.liquidationExpensesList = data;
         });
     }
-
   }
-  selectPeriodChange(id:any){
-    this.router.navigate([`/liquidation-expense/${id}`])
-    this.loadList(id)
+  selectPeriodChange(id: any) {
+    this.router.navigate([`/liquidation-expense/${id}`]).then(() => {
+      this.loadList(id);
+    })
+  }
+
+  closeLiquidation(id: number | null) {
+    if (id) {
+      console.log(id);
+      this.liquidationExpensesService.putCloseLiquidation(id).subscribe({
+        next: () => {
+          console.log('Liquidación cerrada exitosamente');
+          this.loadList(this.id);
+        },
+      });
+    }
+  }
+  closeLiquidationPeriod(id: number | null) {
+    console.log(id);
+
+    if (id) {
+      this.liquidationExpensesService
+        .putCloseLiquidationExpensesPeriod(id)
+        .subscribe({
+          next: () => {
+            this.loadList(this.id);
+          },
+        });
+    }
   }
 }
