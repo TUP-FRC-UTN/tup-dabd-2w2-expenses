2a40ebbc86d077e6e9758cfdff1b9fb31bfc8a9e
114007-Bima-Santiago <114007@tecnicatura.frc.utn.edu.ar>
2024-10-26 18:23:08 -0300

    filtro de liquidation-expense-details

--- expenses/src/app/components/liquidacion/liquidation-expense-details/liquidation-expense-details.component.ts
+++ expenses/src/app/components/liquidacion/liquidation-expense-details/liquidation-expense-details.component.ts
@@ -10,6 +10,9 @@ import { FormsModule } from '@angular/forms';
 import { InfoModalComponent } from '../../modals/info-modal/info-modal.component';
 import { NgModalComponent } from '../../modals/ng-modal/ng-modal.component';
 import { BillService } from '../../../services/bill.service';
+import { Bill } from '../../../models/bill';
+import { CategoryService } from '../../../services/category.service';
+import Category from '../../../models/category';
 
 @Component({
   selector: 'app-liquidation-expense-details',
@@ -33,7 +36,11 @@ export class LiquidationExpenseDetailsComponent implements OnInit{
   private readonly route = inject(ActivatedRoute);
   private readonly router = inject(Router);
   private modalService = inject(NgbModal);
+  private readonly categoryService = inject(CategoryService);
+
   liquidationExpense: LiquidationExpense = new LiquidationExpense();
+  bills: Bill[] = [];
+  categories: Category[] = [];
 
   currentPage = 1;
   itemsPerPage = 10;
@@ -41,28 +48,48 @@ export class LiquidationExpenseDetailsComponent implements OnInit{
   totalPages = 0;
   pages: number[] = [];
 
+  category: number | null = null;
+  period: number | null = null;
+  type: number | undefined = undefined;
+
+  typeFilter: string = '';
 
   ngOnInit(): void {
     this.loadLiquidationExpenseDetails();
+    this.loadCategories();
+  }
+
+  private loadCategories() {
+    this.categoryService.getAllCategories().subscribe(data => {
+      this.categories = data;
+    })
   }
 
   private loadLiquidationExpenseDetails() {
     this.route.paramMap.subscribe(params => {
+      const periodId = params.get('period_id');
       const id = params.get('id');
       if (id && !isNaN(Number(id))) {
         this.service.getById(Number(id)).subscribe((data: LiquidationExpense) => {
           this.liquidationExpense = data;
+          this.type = data.bill_type?.bill_type_id
+
+          if (periodId && !isNaN(Number(periodId))) {
+            this.period = parseInt(periodId);
+          }
+
+          this.getBills(this.itemsPerPage, this.currentPage, this.period, this.category, this.type, "ACTIVE")
         });
       }
     });
-
-    this.liquidationExpense.liquidation_expenses_details.forEach(detaill => {})
   }
 
-  private getBills(itemsPerPage: number, page: number, period: number, category: number, type: number, status: string) {
-    this.billsService.getAllBillsPaged(itemsPerPage, page, period, category, type, status).subscribe(data => {
-      // this.liquidationExpense.liquidation_expenses_details.bills = data;
-    })
+  private getBills(itemsPerPage: number, page: number, period: number | null, category: number | null, type: number | undefined, status: string) {
+    if (type != undefined) {
+      this.billsService.getAllBillsPaged(itemsPerPage, page -1, period, category, type, status).subscribe(data => {
+        this.bills = data.content;
+      })
+    } else console.log("No hay un tipo de expensa definido");
   }
 
 
@@ -127,5 +154,20 @@ export class LiquidationExpenseDetailsComponent implements OnInit{
 
     if (id == null) return;
     this.router.navigate([`gastos/modificar/${id}`])
+  }
+
+  selectFilter(text:string){
+    this.typeFilter=text
+  }
+
+  handleCategoryChange(id: number) {
+    this.category = id;
+    this.getBills(this.itemsPerPage, this.currentPage, this.period, this.category, this.type, "ACTIVE")
+  }
+
+  clean() {
+    this.category = null;
+    this.typeFilter = '';
+    this.getBills(this.itemsPerPage, this.currentPage, this.period, this.category, this.type, "ACTIVE")
   }
 }
