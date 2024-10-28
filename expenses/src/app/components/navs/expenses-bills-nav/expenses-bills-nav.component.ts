6d94b40f105d30a3a9ee044d86bca99a46c3277b
Ramiro Sanchez Solano <164379646+113855-Sanchez@users.noreply.github.com>
2024-10-28 11:11:21 -0300

    Revert "feature/bills"

--- expenses/src/app/components/navs/expenses-bills-nav/expenses-bills-nav.component.ts
+++ expenses/src/app/components/navs/expenses-bills-nav/expenses-bills-nav.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
 
 @Component({
-  selector: 'app-expenses-expenses_bills-nav',
+  selector: 'app-expenses-bills-nav',
   standalone: true,
   imports: [
     RouterLink
