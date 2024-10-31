5e3076a1fc52b28fe04b1e783cf2e6c1c5fbef79
114375_ARROYO_NICOLAS <114375@tecnicatura.frc.utn.edu.ar>
2024-10-31 19:00:12 -0300

    refactorizacion general

--- expenses/src/app/app.component.ts
+++ expenses/src/app/app.component.ts
@@ -1,15 +1,15 @@
-import { AddChargeComponent } from './components/cargo/add-charge/add-charge.component';
+import { AddChargeComponent } from './components/cargo/expenses-add-charge/add-charge.component';
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
-import { ListChargesComponent } from './components/cargo/list-charges/list-charges.component';
+import { ListChargesComponent } from './components/cargo/expenses-list-charges/list-charges.component';
 import {NavigationMenuComponent} from "./components/routes/navigation-menu/navigation-menu.component";
 import {NgbModalModule} from "@ng-bootstrap/ng-bootstrap";
-import { ToastsContainer } from 'ngx-dabd-grupo01';
+import {NavbarComponent, SidebarComponent, ToastsContainer} from 'ngx-dabd-grupo01';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, NavigationMenuComponent, NgbModalModule,ToastsContainer],
+  imports: [RouterOutlet, RouterModule, NgbModalModule, ToastsContainer, NavbarComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
