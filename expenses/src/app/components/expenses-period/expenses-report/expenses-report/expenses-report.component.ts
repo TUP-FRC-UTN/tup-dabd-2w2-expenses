a0f86d6d45edf6080ad21c97b444f6e626ac4ad6
114142_LENTINI <114142@tecnicatura.frc.utn.edu.ar>
2024-11-07 14:36:45 -0300

    reportes

--- expenses/src/app/components/expenses-period/expenses-report/expenses-report/expenses-report.component.ts
+++ expenses/src/app/components/expenses-period/expenses-report/expenses-report/expenses-report.component.ts
@@ -1,47 +1,88 @@
-import { Component, inject, input, Input, OnInit, SimpleChanges } from '@angular/core';
-import { ExpenseServiceService} from "../../../../services/expense.service";
-import { ActivatedRoute, Router, RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { PeriodSelectComponent} from "../../../selects/period-select/period-select.component";
+import {Component, inject} from '@angular/core';
+import {ExpenseServiceService} from "../../../../services/expense.service";
+import {RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {PeriodSelectComponent} from "../../../selects/period-select/period-select.component";
 import {CommonModule, DatePipe} from '@angular/common';
-
 import {NgPipesModule} from "ngx-pipes";
-import {
-  TableColumn, TableComponent, ConfirmAlertComponent,
-
-  MainContainerComponent,
-  ToastService, TableFiltersComponent, Filter, FilterConfigBuilder, FilterOption, SelectFilter
-} from "ngx-dabd-grupo01" ;
+import {MainContainerComponent, TableComponent, TableFiltersComponent} from "ngx-dabd-grupo01";
 import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
-import Expense from "../../../../models/expense";
+import {
+  BarController,
+  BubbleController,
+  ChartData,
+  ChartType,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController
+} from 'chart.js';
+import {BaseChartDirective} from "ng2-charts";
+import Chart from 'chart.js/auto';
 
+Chart.register(BarController, PieController, RadarController, LineController, PolarAreaController, DoughnutController, BubbleController, ScatterController);
 @Component({
   selector: 'app-expenses-report',
   standalone: true,
   imports: [
-    CommonModule, RouterModule, FormsModule, PeriodSelectComponent, TableComponent, NgPipesModule, MainContainerComponent, TableFiltersComponent
+    CommonModule, RouterModule, FormsModule, PeriodSelectComponent, TableComponent, NgPipesModule, MainContainerComponent, TableFiltersComponent, BaseChartDirective
   ],
   providers: [DatePipe, NgbActiveModal],
   templateUrl: './expenses-report.component.html',
   styleUrl: './expenses-report.component.css'
 })
 export class ExpensesReportComponent {
-  periods: FilterOption[] = [];
-  lotss: FilterOption[] = []
-  types: FilterOption[]= []
-  filterConfig: Filter[] = [
-    new SelectFilter('Lote','lot','Seleccione un lote',this.lotss),
-    new SelectFilter('Tipo de lote','type','Seleccione un tipo de lote',this.types),
-    new SelectFilter('Periodos','period','Seleccione un periodo',this.periods)
-  ]
-  expenses: Expense[] = [];
-  searchTerm: string = "";
+  public barChartType: ChartType = 'bar';
+  public barChartData: ChartData<'bar'> = {
+    labels: [],
+    datasets: [
+      { data: [], label: 'Expensas Totales por Lote' }
+    ]
+  };
+  public pieChartType: ChartType = 'pie';
+  public pieChartData: ChartData<'pie'> = {
+    labels: [],
+    datasets: [
+      { data: [], label: 'Expensas Totales por Lote' }
+    ]
+  };
 
-  onFilterTextBoxChanged($event: Event) {
 
+  service : ExpenseServiceService = inject(ExpenseServiceService);
+
+  ngOnInit(): void {
+    this.loadExpenseData();
   }
-  filterChange($event: Record<string, any>) {
 
+  loadExpenseData(): void {
+    this.service.getExpensesByLot().subscribe(expenses => {
+      console.log(expenses);
+      const lotNumbers = expenses.map(expense => expense.plotNumber);
+      const totalAmounts = expenses.map(expense => expense.totalAmount);
+
+      // Reasignar el objeto completo para forzar la detección de cambios
+      this.barChartData = {
+        labels: lotNumbers,
+        datasets: [
+          { data: totalAmounts, label: 'Distribución de Expensas por Lote' }
+        ]
+      }
+    });
+    this.service.getLotPercentage().subscribe(p => {
+      console.log(p);
+     const por = p.map(pe => {
+        pe = pe * 100 ;
+        return pe
+      })
+      this.pieChartData = {
+       labels: this.barChartData.labels,
+        datasets: [
+          {data: por , label: "Contribución de Expensas por Lote (%)" },
+        ]
+      }
+    })
   }
 
 
